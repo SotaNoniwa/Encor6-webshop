b570584bb1330daccfd4b6bd31f4ebb9d75761b9
Sota Noniwa <surfersou@gmail.com>
2024-04-13 19:13:22 +0200

    update

--- pages/api/restricted.ts
+++ pages/api/restricted.ts
@@ -1,5 +1,5 @@
 // import { getServerSession } from "next-auth/next";
-// import { authOptions } from "../../app/api/auth/[...nextauth]";
+// import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 // const restrictedHandler = async (req, res) => {
 //   const session = await getServerSession(req, res, authOptions);
